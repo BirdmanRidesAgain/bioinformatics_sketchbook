dea6dec16ff1db5516ccee100f35ce28a7f52666
Keiler Collier <82856657+BirdmanRidesAgain@users.noreply.github.com>
2023-12-07 18:57:31 +0400

    Update psmc_input_file_prep.sh
    Edited the variant-calling step to reflect what worked for the stone curlew dataset. Immediately prior version may have merit to retain.

--- psmc/psmc_input_file_prep.sh
+++ psmc/psmc_input_file_prep.sh
@@ -17,7 +17,7 @@ UPPER_BOUND=100
 
 minimap2 -t $THREADS -x map-ont $ASSEMBLY $READS | samtools sort | samtools view -bS > ${PROJECT_NAME}_ONT_remapped.bam
 MAPFILE=${PROJECT_NAME}_ONT_remapped.bam
-bcftools mpileup -C50 -uf $ASSEMBLY $MAPFILE | bcftools view -c - | vcfutils.pl vcf2fq -d $LOWER_BOUND -D $UPPER_BOUND | gzip > ${PROJECT_NAME}.fq.gz
+bcftools mpileup -Q 30 -q 30 -f $ASSEMBLY $MAPFILE | bcftools call -c | vcfutils.pl vcf2fq -d $LOWER_BOUND -D $UPPER_BOUND | gzip > ${PROJECT_NAME}.fq.gz
 
 ### Command used for experimentation purposes
 #bcftools mpileup -Ou -f $ASSEMBLY -a FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR $MAPFILE | bcftools call -mv -Oz -f GQ,GP -o Burhinus_ONT_remapped_sort.vcf.gz
