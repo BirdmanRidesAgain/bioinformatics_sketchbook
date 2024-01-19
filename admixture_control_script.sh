89bdb48c69851198aedfbbff068ceb2d79cb83ea
Keiler Collier <82856657+BirdmanRidesAgain@users.noreply.github.com>
2024-01-19 10:21:20 +0400

    Update admixture_control_script.sh
    Added in a portion of the script to collect log likelihood values

--- admixture_control_script.sh
+++ admixture_control_script.sh
@@ -28,23 +28,53 @@ ADMIXTURE_OUTDIR=$WD/${PREFIX}_output
 echo "Beginning admixture runs for K=1 to K=$MAX_K"
 
 for i in $(seq 1 $MAX_K); do
-    echo "Beginning K=$i"
+    echo "  Beginning K=$i"
     mkdir -p $ADMIXTURE_OUTDIR/K${i}
     K_DIR=$ADMIXTURE_OUTDIR/K${i}
+
     # repeat 30x, each with a different seed
     for j in $(seq 1 $NUM_ITERATIONS); do
-        echo "  K=$i, replicate $j"
+        echo "      K=$i, replicate $j"
         admixture --cv $INPUT_BED -j${THREADS} --seed=$j $i > ${PREFIX}.K${i}.rep${j}.log
 
         # move output files to the appropriate directory
-        mv ${PREFIX}.${i}.rep${j}.log $K_DIR
-        mv ${PREFIX}.${i}*.P $K_DIR/${PREFIX}.K${i}.rep${j}.P
-        mv ${PREFIX}.${i}*.Q $K_DIR/${PREFIX}.K${i}.rep${j}.Q
-    done
-    echo "Finished K=$i"
-done
+        LOG=${PREFIX}.K${i}.rep${j}.log
+        PQ_PREFIX=${PREFIX}.K${i}.rep${j}
 
+        mv $LOG $K_DIR
+        mv ${PREFIX}.${i}*.P $K_DIR/${PQ_PREFIX}.P
+        mv ${PREFIX}.${i}*.Q $K_DIR/${PQ_PREFIX}.Q
+    done
+    echo "  Finished K=$i"
+done
 echo "Finished admixture runs for K=1 to K=$MAX_K"
+
+
+##################################
+# GET LOG LIKELIHOOD VALUES FROM ALL VALUES OF K
+# Loop through each value of K
+echo "Collecting log likelihood values for K=1 to K=$MAX_K"
+for i in $(seq 1 $MAX_K); do
+    echo "  Collecting log likelihood values for K=$i"
+    # find the log likelihood value for each replicate
+    for j in $(seq 1 $NUM_ITERATIONS); do
+        echo "      K=$i, replicate $j"
+        LOGLIKE=`cat $ADMIXTURE_OUTDIR/K${i}/${PREFIX}.K${i}.rep${j}.log | grep -v "(delta)" | grep Loglikelihood | awk '{print $NF}'`
+        echo $LOGLIKE >> $ADMIXTURE_OUTDIR/loglike.K${i}.txt
+    done
+    echo "  Finished collecting log likelihood values for K=$i"
+done
+echo "Finished collecting log likelihood values for K=1 to K=$MAX_K"
+
+
+##################################
+# FINDING BEST K WITH EVANNO METHOD
+    # do stuff here.
+
+
+##################################
+# TERMINATE PROGRAM
+echo "Finished admixture_control_script.sh"
 
 
 
