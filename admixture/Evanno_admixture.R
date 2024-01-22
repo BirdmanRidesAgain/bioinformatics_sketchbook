67da08d50a61e9c3701177ab2b7c04950b162c33
Keiler Collier <82856657+BirdmanRidesAgain@users.noreply.github.com>
2024-01-22 15:42:13 +0400

    Update Evanno_admixture.R
    Added a couple of lines that output a csv file that corresponds to the Evanno et al. plots.

--- admixture/Evanno_admixture.R
+++ admixture/Evanno_admixture.R
@@ -21,7 +21,7 @@ if (length(ARGS)==0)
 } else if(length(ARGS)==1)
 {
   message("No prefix was provided. 'output' used for all outfiles.")
-  ARGS[2] = "output"
+  ARGS[2] <- "output"
 }
   
   ###################################################
@@ -45,6 +45,8 @@ if (length(ARGS)==0)
   message(paste(" Output prefix:", ARGS[2]))
   message("")
   
+  table_loc <- ARGS[1]
+  output_prefix <- ARGS[2]
   
   ###################################################
   ### HELPER FUNCTIONS ###
@@ -59,7 +61,6 @@ if (length(ARGS)==0)
   message("Reading in log likelihood file:")
   message("")
   
-  table_loc <- ARGS[1]
   table_loc <- "loglike.K1-K9.txt"
   #q_cols <- c("K1","K2")
   loglikes <- readr::read_table(file = table_loc) %>%
@@ -73,7 +74,7 @@ if (length(ARGS)==0)
 # Get table of derivatives
   num_K = nrow(mean_loglikes) # used to set the number of loops to run
   # Create the 'derivs' tibble. We have to create this one separately, because applying the functions across rows of K values results in NA for K=1
-  derivs <- tibble(ln_1st_deriv = NA, ln_2nd_deriv = NA, delta_K = NA) # We manually assign 'NAs' for K=1
+  derivs <- tibble(ln_1st_deriv = NA, absolute_val_ln_2nd_deriv = NA, delta_K = NA) # We manually assign 'NAs' for K=1
   
   # Calculate and add rows for K2-K(Max_K-1)
     # The loop relies on adding values to i in order to add/subtract derivative values. See get_1st_deriv() for details.
@@ -84,7 +85,7 @@ if (length(ARGS)==0)
     
     derivs <- add_row(.data = derivs,
             ln_1st_deriv = new_1st_deriv,
-            ln_2nd_deriv = new_2nd_deriv,
+            absolute_val_ln_2nd_deriv = new_2nd_deriv,
             delta_K = new_delta_K)
   }
 
@@ -92,13 +93,15 @@ if (length(ARGS)==0)
 # This is because there's no larger mean_loglike to subtract K='num_K''s mean_loglike from. Therefore, we set it back to 'NA'.
   derivs$ln_1st_deriv[num_K] <- NA 
   
-##############################################
-### C_BIND derivs AND mean_loglikes ###
-##############################################
-mean_loglikes <- bind_cols(mean_loglikes, derivs)  
+##########################################################
+### CREATE UNIFIED DATA FRAME AND SAVE TO LOCAL DISK ###
+##########################################################
+mean_loglikes <- bind_cols(mean_loglikes, derivs)  # binds columns and writes back to "mean_loglikes"
+write_csv(mean_loglikes, file = stringr::str_c(output_prefix,"_maxK", num_K,".csv"))
 
+  
 ##############################################
-# Plot the Evanno output
+### PLOT THE EVANNO OUTPUT ###
 ##############################################
 ### OVERLAID CHART - BAD IDEA
 #lines_overlaid <- ggplot(data = mean_loglikes) + 
@@ -108,9 +111,7 @@ mean_loglikes <- bind_cols(mean_loglikes, derivs)
 #    geom_point(aes(x = num_K, y = delta_K), color = 'blue') +
 #    theme_classic()
   
-mean_ln_only <- ggplot(data = mean_loglikes, aes(x = num_K, y = mean_ln)) +  
-  #title(main = "") +
-  axis_theme +
+mean_ln_only <- ggplot(data = mean_loglikes, aes(x = num_K, y = mean_ln)) + 
   xlab(label = "Value of K" ) +
   ylab(label = "Mean LnP(K)") +
   scale_x_continuous(breaks = seq.int(from = 0, to = 9, by = 1)) +
@@ -137,7 +138,6 @@ mean_ln_deltaK_plot <- mean_ln_only + delta_K_only
 ###################################################
 ### SAVE PLOT TO LOCAL FILESYSTEM ###
 ###################################################
-output_prefix <- ARGS[2]
 ggsave(
   stringr::str_c(output_prefix,"_maxK", num_K, ".pdf"),
   plot = mean_ln_deltaK_plot,
