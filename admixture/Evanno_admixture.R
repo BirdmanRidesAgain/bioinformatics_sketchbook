8cda6007c75345654c867c1c4983a794e33be17c
Keiler Collier <82856657+BirdmanRidesAgain@users.noreply.github.com>
2024-01-23 10:40:45 +0400

    Update Evanno_admixture.R
    Quality-of-life and readability edits. Specifically, all stringr::str_c() statements are written to variables _before_ inserting the variables into their commands.

--- admixture/Evanno_admixture.R
+++ admixture/Evanno_admixture.R
@@ -5,7 +5,6 @@
 ###################################################
 # Evanno_admixture.R
 # KCollier 22 Jan 2024
-
 # This is a command-line-usable script that takes a list of log likelihood values from admixture.
 # It summarizes them and finds the best K value with the Evanno et al. method.
 
@@ -25,13 +24,9 @@ if (length(ARGS)==0)
 }
 
 ###################################################
-### CHECKS FOR ALL NECESSARY PACKAGES ###
+### SET VARS AND LOAD PACKAGES ###
 ###################################################
-# Check if tidyverse is installed. Install if it is not present.
-#if (!requireNamespace("tidyverse", quietly = FALSE))
-#{
-#  install.packages("tidyverse")
-#}
+  # libraries with a single required function (stringr, readr) are called as needed with namespace resolution operator
 library(ggplot2)
 library(dplyr)
 library(patchwork)
@@ -40,12 +35,12 @@ message("All necessary packages loaded.")
 message("")
 
 message("Parameters interpreted as:")
-message(paste(" Input loglikelihoods.txt:", ARGS[1]))
-message(paste(" Output prefix:", ARGS[2]))
+table_loc <- ARGS[1]
+message(paste(" Input loglikelihoods.txt:", table_loc))
+output_prefix <- ARGS[2]
+message(paste(" Output prefix:", output_prefix))
 message("")
 
-table_loc <- ARGS[1]
-output_prefix <- ARGS[2]
 
 ###################################################
 ### HELPER FUNCTIONS ###
@@ -59,8 +54,6 @@ get_1st_deriv <- function(index) {
 ###################################################
 message("Reading in log likelihood file:")
 message("")
-
-#q_cols <- c("K1","K2")
 loglikes <- readr::read_table(file = table_loc) %>%
   mutate(ln = log(abs(log_likelihood)) * -1)
 
@@ -72,14 +65,13 @@ mean_loglikes <- loglikes %>%
     mean_ln = mean(ln),
     std_dev = sd(ln))
 
-# Get table of derivatives
+# Get table of derivatives - 'derivs' tibble.
+  # We have to create this one separately, because applying the functions across rows of K values results in NA for K=1
 num_K <- nrow(mean_loglikes) # used to set the number of loops to run
-
-# Create the 'derivs' tibble. We have to create this one separately, because applying the functions across rows of K values results in NA for K=1
 derivs <- tibble(ln_1st_deriv = NA, absolute_val_ln_2nd_deriv = NA, delta_K = NA) # We manually assign 'NAs' for K=1
 
 # Calculate and add rows for K2-K(Max_K-1)
-# The loop relies on adding values to i in order to add/subtract derivative values. See get_1st_deriv() for details.
+# The loop relies on adding values to i in order to add/subtract derivative values. See helper function 'get_1st_deriv()' for details.
 for (i in 1:(num_K-1)) {
   new_1st_deriv <- get_1st_deriv(i)
   new_2nd_deriv <- abs(get_1st_deriv(i + 1) - new_1st_deriv)
@@ -90,22 +82,23 @@ for (i in 1:(num_K-1)) {
                     absolute_val_ln_2nd_deriv = new_2nd_deriv,
                     delta_K = new_delta_K)
 }
-
-# The first deriv. for K='num_K' is technically calculated, but is an invalid result.
-# This is because there's no larger mean_loglike to subtract K='num_K''s mean_loglike from. Therefore, we set it back to 'NA'.
+# The first deriv. for the max_K is technically calculated, but is an invalid result and is manually set back to 'NA'.
+  # This is because there's no larger mean_loglike to subtract the max_K's mean_loglike from. 
 derivs$ln_1st_deriv[num_K] <- NA 
 
 ##########################################################
 ### CREATE UNIFIED DATA FRAME AND SAVE TO LOCAL DISK ###
 ##########################################################
 mean_loglikes <- bind_cols(mean_loglikes, derivs)  # binds columns and writes back to "mean_loglikes"
-readr::write_csv(mean_loglikes, file = stringr::str_c(output_prefix,"_maxK", num_K,".csv"))
+csv_output_name <- stringr::str_c(output_prefix,"_maxK", num_K,".csv")
+readr::write_csv(mean_loglikes, file = csv_output_name)
 
 
 ##############################################
 ### PLOT THE EVANNO OUTPUT ###
 ##############################################
 ### OVERLAID CHART - BAD IDEA
+  # This was the output shown in the excel file. It's difficult to interpret, given the vastly different Y-axis scales.
 #lines_overlaid <- ggplot(data = mean_loglikes) + 
 #    geom_line(aes(x = num_K, y = mean_ln), color = 'red') +
 #    geom_point(aes(x = num_K, y = mean_ln), color = 'red') + 
@@ -135,16 +128,21 @@ delta_K_only <- ggplot(data = mean_loglikes, aes(x = num_K, y = delta_K)) +
   theme_classic()
 
 # Use patchwork to display them side-by-side
+mean_ln_deltaK_title <- stringr::str_c(output_prefix, "Evanno et al. plots")
+mean_ln_deltaK_subtitle <- stringr::str_c("K1 through K", num_K)
+
 mean_ln_deltaK_plot <- mean_ln_only + delta_K_only +
-  plot_annotation(title = stringr::str_c(output_prefix, " Evanno et al. plots"),
-                  subtitle = stringr::str_c("K1 through K", num_K),
+  plot_annotation(title = mean_ln_deltaK_title,
+                  subtitle = mean_ln_deltaK_subtitle,
                   tag_levels = 'A') & theme(text = element_text(face = "bold", size = 14))
 
 ###################################################
 ### SAVE PLOT TO LOCAL FILESYSTEM ###
 ###################################################
+graphic_output_name <- stringr::str_c(output_prefix,"_maxK", num_K, ".pdf")
+
 ggsave(
-  stringr::str_c(output_prefix,"_maxK", num_K, ".pdf"),
+  filename = graphic_output_name,
   plot = mean_ln_deltaK_plot,
   device = "pdf",
   width = 500,
@@ -152,4 +150,5 @@ ggsave(
   units = "mm",
   dpi = 300,
 )
+
 message("Program end.")
